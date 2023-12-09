33c9429fc4c05d82313bf7552d1f172e69ca532e
Joel <30225752+jayabastillas@users.noreply.github.com>
2023-12-09 22:23:59 +0800

    Pushed Login page UIX

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/provider";
+import { extendTheme } from "@chakra-ui/theme-utils";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import themes from "./themes.js";
+import styles from "./styles.js";
+
+const theme = extendTheme({ themes, styles });
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <BrowserRouter basename="/">
+            <ChakraProvider theme={theme}>
+                <App />
+            </ChakraProvider>
+        </BrowserRouter>
+    </React.StrictMode>
+);
